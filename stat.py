07bf74bfbb82fbc6c146f99f0d60cdb299580047
zhuty18 <zhuty18@mails.tsinghua.edu.cn>
2020-10-16 22:04:29 +0800

    10.16 2204 update ...never more

--- stat.py
+++ stat.py
@@ -5,13 +5,13 @@ unfin=[]
 
 def writeResults(f):
     f.write("### To Be Continued\n\n")
-    f.write("|名称|位置|字数|\n|:-|:-|\n")
+    f.write(" | 名称 | 位置 | 字数 | \n | :- | :- | \n")
     for i in unfin:
-        f.write(i+"\n")
+        f.write(i.replace("|"," | ")+"\n")
     f.write("\n### Finished\n\n")
-    f.write("|名称|位置|字数|\n|:-|:-|\n")
+    f.write(" | 名称 | 位置 | 字数 | \n | :- | :- | \n")
     for i in finish:
-        f.write(i+"\n")
+        f.write(i.replace("|"," | ")+"\n")
 
 def write(info,fin):
     if fin:
