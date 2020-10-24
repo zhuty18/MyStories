6980746c7e529d0fc229b66d26583c6df343b7fb
zhuty18 <zhuty18@mails.tsinghua.edu.cn>
2020-10-24 13:29:28 +0800

    10.24 13:29 update

--- wc.py
+++ wc.py
@@ -8,6 +8,7 @@ import matplotlib.font_manager as fm
 class WordPic:
     def __init__(self,path):
         self.font=fm.FontProperties(fname='myfont.ttf',size=15)
+        jieba.load_userdict('mydict.dic')
         self.drawFiles(path)
     def drawFiles(self,path):
         list=os.listdir(path)
@@ -23,15 +24,19 @@ class WordPic:
             f=open(path,'r',encoding='utf-8')
             str=f.read()
             keywords=jieba.analyse.extract_tags(str,withWeight=True,topK=50)
+            print(name.replace('.md',''))
+            print(keywords)
+            print()
             fre={keyword[0]:keyword[1] for keyword in keywords}
             wc=wordcloud.WordCloud(font_path="myfont.ttf",width=600,height=400)
             wc.fit_words(fre)
-            plt.imshow(wc)
-            plt.title(path.replace('.md',''),fontproperties=self.font)
-            plt.ion()
-            plt.pause(1)
-            plt.close()
-            #wc.to_file(path.replace('.md','.png'))
+#            plt.imshow(wc)
+#            plt.title(path.replace('.md',''),fontproperties=self.font)
+#            plt.ion()
+#            plt.pause(1)
+#            plt.close()
+#            wc.to_file(path.replace('.md','.png'))
+            os.remove(path.replace('.md','.png'))
 
 path = os.getcwd()
 WordPic(path)
