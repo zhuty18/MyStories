7852402f2eab011c7a9b3ed945b0d794e61b72a7
zhuty18 <zhuty18@mails.tsinghua.edu.cn>
2020-10-26 13:13:40 +0800

    10.26 13:13 update

--- AutoCommit-Linux.sh
+++ AutoCommit-Linux.sh
@@ -1,3 +1,4 @@
 python3 prepare.py
 git add .
 git commit -m "$(date "+%m.%d %H:%M") update"
+git push
--- AutoCommit-Windows.bat
+++ AutoCommit-Windows.bat
@@ -2,3 +2,4 @@ python prepare.py
 git add .
 set h=%time:~0,5%
 git commit -m "%date:~3,2%.%date:~6,2% %h: =0% update"
+git push
